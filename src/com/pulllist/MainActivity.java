27cab8cdade35e54521a4acb51a516d7b0b69be4
Alan Wright <alanwright47@gmail.com>
2012-12-02 15:52:10 -0500

    Initial Commit
    Initial Commit
    -Pulls xml feed from Comixology
    -Runs in background (async)
    -Parses Title and link
    -List View of just title, links to webpage

---
+++ src/com/pulllist/MainActivity.java
@@ -0,0 +1,159 @@
+package com.pulllist;
+
+import java.util.List;
+
+import com.pulllist.data.RssItem;
+import com.pulllist.listeners.ListListener;
+import com.pulllist.util.RssReader;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+ 
+public class MainActivity extends Activity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //final ProgressBar progress = (ProgressBar)findViewById(R.id.progress);
+        //final TextView    textview = (TextView)findViewById(R.id.text);
+        
+        //progress.setVisibility(View.GONE);
+        //textview.setVisibility(View.GONE);
+        
+        new RSSRead(this).execute();
+        
+//			Context context = getApplicationContext();
+//			CharSequence text = "Hello toast! " + temp.rssItems;
+//			int duration = Toast.LENGTH_LONG;
+//
+//			Toast toast = Toast.makeText(context, text, duration);
+//			toast.show();
+    }
+    
+    private class RSSRead extends AsyncTask<Void, Integer, Void>{
+        Activity parent;
+        ListView itcItems;
+        List<RssItem> rssItems;
+        ArrayAdapter<RssItem> adapter;
+        
+    	RSSRead(MainActivity parent){
+    		this.parent = parent;
+    	}
+        
+        // A callback method executed on UI thread on starting the task
+        @Override
+        protected void onPreExecute() {
+            // Getting reference to the TextView tv_counter of the layout activity_main
+
+        }
+ 
+        // A callback method executed on non UI thread, invoked after
+        // onPreExecute method if exists
+        @Override
+        protected Void doInBackground(Void... params) {
+        	/* Load timezone. this is very slow - may take up to 3 seconds. */
+          //publishProgress();
+          
+          
+          
+        	try {
+    			// Create RSS reader
+    			RssReader rssReader = new RssReader("http://feeds.feedburner.com/ComixologyDigitalComics");
+    			// Get a ListView from main view
+    			itcItems = (ListView) findViewById(R.id.listMainView);
+    			
+    			rssItems = rssReader.getItems();
+    			
+    			// Create a list adapter
+    			adapter = new ArrayAdapter<RssItem>(parent,android.R.layout.simple_list_item_1, rssItems);
+    			
+    			
+    		} catch (Exception e) {
+    			Log.e("ITCRssReader", e.getMessage());
+    		}
+          
+          return null;
+        }
+ 
+        // A callback method executed on UI thread, invoked by the publishProgress()
+        // from doInBackground() method
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+ 
+        }
+ 
+        // A callback method executed on UI thread, invoked after the completion of the task
+        @Override
+        protected void onPostExecute(Void result) {
+        	// Set list adapter for the ListView
+			itcItems.setAdapter(adapter);
+			
+			// Set list view item click listener
+			itcItems.setOnItemClickListener(new ListListener(rssItems, parent));
+        }
+       
+    }
+}
+
+//package com.pulllist;
+//
+//import android.app.Activity;
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.os.StrictMode;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//
+//import com.pulllist.data.RssItem;
+//import com.pulllist.listeners.ListListener;
+//import com.pulllist.util.RssReader;
+//
+///**
+// * Main application activity.
+// * 
+// * @author ITCuties
+// *
+// */
+//public class MainActivity extends Activity {
+//	/** 
+//	 * This method creates main application view
+//	 */
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		// Set view
+//		setContentView(R.layout.activity_main);
+//		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//		StrictMode.setThreadPolicy(policy);
+//		
+//		try {
+//			// Create RSS reader
+//			RssReader rssReader = new RssReader("http://feeds.feedburner.com/ComixologyDigitalComics");
+//			// Get a ListView from main view
+//			ListView itcItems = (ListView) findViewById(R.id.listMainView);
+//			
+//			// Create a list adapter
+//			ArrayAdapter<RssItem> adapter = new ArrayAdapter<RssItem>(this,android.R.layout.simple_list_item_1, rssReader.getItems());
+//			// Set list adapter for the ListView
+//			itcItems.setAdapter(adapter);
+//			
+//			// Set list view item click listener
+//			itcItems.setOnItemClickListener(new ListListener(rssReader.getItems(), this));
+//			
+//		} catch (Exception e) {
+//			Log.e("ITCRssReader", e.getMessage());
+//		}
+//		
+//	}
+//	
+//	
+//}
